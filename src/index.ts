7a4f4d4155dd1688c67ba5f723a0d455e8c52535
hasan <hassanzohdy@gmail.com>
2022-11-04 08:52:27 +0200

    "Fixed exports"

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import cache from "./CacheManager";
 
-export * from "./types";
-export * from "./config";
 export { CacheManager } from "./CacheManager";
+export * from "./config";
 export { default as BaseCacheEngine } from "./drivers/BaseCacheEngine";
-export { default as PlainLocalStorageDriver } from "./drivers/PlainLocalStorageDriver";
 export { default as EncryptedLocalStorageDriver } from "./drivers/EncryptedLocalStorageDriver";
+export { default as EncryptedSessionStorageDriver } from "./drivers/EncryptedSessionStorageDriver";
+export { default as PlainLocalStorageDriver } from "./drivers/PlainLocalStorageDriver";
+export { default as PlainSessionStorageDriver } from "./drivers/PlainSessionStorageDriver";
+export { default as RunTimeDriver } from "./drivers/RunTimeDriver";
+export * from "./types";
 
 export default cache;
